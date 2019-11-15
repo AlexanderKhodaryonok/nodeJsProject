07b8bc61e142d96cc4fdabeecfcdb1c2f33e3e4b
AlexanderKhodaryonok <alexanderkhodaryonok@gmail.com>
2019-11-15 17:53:31 +0300

    add mongoDB

--- models/sortOfVino.js
+++ models/sortOfVino.js
@@ -1,84 +1,15 @@
-const uuid = require('uuid/v4');
-const fs = require('fs');
-const path = require('path');
+const { Schema, model } = require('mongoose');
 
-class SortOfVino {
-  constructor(title, price, img){
-    this.title = title;
-    this.price = price;
-    this.img = img;
-    this.id = uuid();
-  }
+const sortOfVino = new Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+  img: String
+});
 
-  toJSON() {
-    return {
-      title: this.title,
-      price: this.price,
-      img: this.img,
-      id: this.id
-    }
-  };
-
-  static async update(sort) {
-    const sorts = await   SortOfVino.getAll();
-
-    const idx = sorts.findIndex(s => s.id === sort.id);
-    sorts[idx] = sort;
-    
-    return new Promise((resolve, reject) => {
-      fs.writeFile(
-        path.join(__dirname, '..', 'data', 'vino.json'),
-        JSON.stringify(sorts),
-        (error) => {
-          if(error) {
-            reject(error)
-          } else {
-            resolve();
-          }
-        }
-      )
-    })
-  }
-
-  async save () {
-    const sorts = await SortOfVino.getAll();
-    sorts.push(this.toJSON());
-
-    return new Promise((resolve, reject) => {
-      fs.writeFile(
-        path.join(__dirname, '..', 'data', 'vino.json'),
-        JSON.stringify(sorts),
-        (error) => {
-          if(error) {
-            reject(error)
-          } else {
-            resolve();
-          }
-        }
-      )
-    })
-  }
-
-  static getAll() {
-    return new Promise((resolve, reject) => {
-      fs.readFile(
-        path.join(__dirname, '..', 'data', 'vino.json'),
-        'utf-8',
-        (error, content) => {
-          if(error) {
-            reject(error)
-          } else {
-            resolve(JSON.parse(content));
-          }
-        }
-      );
-    })
-  }
-
-  static async getById (id) {
-    const sorts = await SortOfVino.getAll();
-    return sorts.find(s => s.id === id);
-  }
-}
-
-module.exports = SortOfVino;
+module.exports = model('SortOfVino', sortOfVino)
--- routes/catalog.js
+++ routes/catalog.js
@@ -4,7 +4,7 @@ const SortOfVino = require('../models/sortOfVino');
 const router = Router();
 
 router.get('/', async (req, res) => {
-  const sorts = await SortOfVino.getAll(); 
+  const sorts = await SortOfVino.find(); 
   res.render('catalog', {
     title: 'Catalog',
     isCatalog: true,
@@ -16,7 +16,7 @@ router.get('/:id/edit', async (req, res) => {
   if (!req.query.allow) {
     return res.redirect('/');
   } else {
-    const sort = await SortOfVino.getById(req.params.id);
+    const sort = await SortOfVino.findById(req.params.id);
     res.render('catalogEdit', {
       title: `Edit`,// ${sort.title}`,
       sort
@@ -25,12 +25,14 @@ router.get('/:id/edit', async (req, res) => {
 });
 
 router.post('/edit', async (req, res) => {
-  await SortOfVino.update(req.body);
+  const { id } = req.body;
+  delete req.body.id;
+  await SortOfVino.findByIdAndUpdate(id, req.body);
   res.redirect("/catalog");
 })
 
 router.get('/:id', async (req, res) => {
-  const sort = await SortOfVino.getById(req.params.id);
+  const sort = await SortOfVino.findById(req.params.id);
   res.render('catalogInnerPage', {
     layout: 'empty',
     title: `Sort`,// ${sort.title}`,
